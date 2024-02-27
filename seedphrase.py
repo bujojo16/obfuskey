06cbeab28ac92a9796461609af1a01e1b52b3435
bujojo <bujojo16@gmail.com>
2024-02-27 15:27:21 +0200

    remove unused methods from seedphrase class

--- seedphrase.py
+++ seedphrase.py
@@ -10,57 +10,11 @@ class Seedphrase:
         self.processed_phrase = []
         self.indexes = []
 
-    def get_processed_phrase(self):
-        return " ".join(self.processed_phrase)
-    
     def get_seedphrase_indexes(phrase, mnemonic):
         indexes = []
         for word in phrase:
             indexes.append(mnemonic.index(word))
         return indexes
-
-    def obfuscate_and_write_output_if_not_already_processed(self, password_list):
-        check = Output.check_output_already_exists(self)
-        if check[0] == False:
-            self.obfuscate_seedphrase(password_list)
-            return Output.create_output_file_and_write_output(self, password_list)
-        elif check[0] == True: 
-            return {'success': False, 'message': check[1]}
-
-    def obfuscate_seedphrase(phrase, password_list):
-        self.work_phrase = phrase
-        self.get_seedphrase_indexes()
-        for password in password_list:
-            self.generate_safe_phrase(password)
-            self.work_phrase = self.processed_phrase
-            self.get_seedphrase_indexes()
-
-    def desobfuscate_seedphrase(self, password_list):
-        self.work_phrase = self.original_phrase
-        self.get_seedphrase_indexes()
-        for password in password_list:
-            self.retrieve_seedphrase(password)
-            self.phrase = self.processed_phrase
-            self.get_seedphrase_indexes()
-
-    def generate_safe_phrase(self, password):
-        safe_phrase = []
-        mnemonic_size = len(self.mnemonic)
-        for index in range(self.size):
-            safe_index = self.indexes[index] + password.offsetList[index]
-            safe_word = self.mnemonic[safe_index%mnemonic_size]
-            safe_phrase.append(safe_word)
-        self.processed_phrase = safe_phrase
-        
-    def retrieve_seedphrase(self, password):
-        retrieved_phrase = []
-        mnemonic_size = len(self.mnemonic)
-        for index in range(self.size):
-            retrieved_index = self.indexes[index] - password.offsetList[index]
-            retrieved_word = self.mnemonic[retrieved_index%mnemonic_size]
-            retrieved_phrase.append(retrieved_word)
-        self.processed_phrase = retrieved_phrase
-
 
     def inspect_parameters_types(seedphrase):
         return (seedphrase.inspect_size() and 
